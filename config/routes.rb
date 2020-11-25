f390a829a52e5fc5d5da066ed8148f384940b20a
hamayun <hamayun.waheed1999@gmail.com>
2020-11-25 21:32:37 +0500

    finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
-  root to: 'articles#index'
-
+  resources :articles do
+    resources :comments
+  end
+  root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
