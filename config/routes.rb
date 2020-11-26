deb0b5178eec570d52f60ec17ca0d8d7a0d9b285
hamayun <hamayun.waheed1999@gmail.com>
2020-11-26 18:51:24 +0500

    adde few gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :tags
   root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
